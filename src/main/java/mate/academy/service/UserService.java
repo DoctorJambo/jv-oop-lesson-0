a30e9e658206562e9533b6c0e2cff0d1224b5019
Vlad <arhipov09090az@gmail.com>
2025-07-06 14:39:47 +0300

    implement methods for users verify

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -8,13 +8,22 @@ public class UserService {
             new User("alice@i.ua", "1234")
     };
 
+    public static User[] getUsers() {
+        return users;
+    }
+
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
+    public User findByEmail(String email, User[] users) {
+        for (User user : users) {
+            if (email.equals(user.getEmail())) {
+                return user;
+            }
+        }
         return null;
     }
 }
